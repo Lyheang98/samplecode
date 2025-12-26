860cf745ae2022f0fa9e8b7f1f8e355496ea9f2c
Rey-Sok <sok2005r@email.com>
2025-12-26 13:47:33 +0700

    Test automatic deployment

--- api/api.js
+++ api/api.js
@@ -2,7 +2,6 @@
 export const API_BASE = "https://moeys-exam-qbfys.ondigitalocean.app"
 export const MOCK_USERNAME = "Staff"
 export const MOCK_PASSWORD = "staffmoeysedtech2025"
-
 // export const API_BASE = "http://127.0.0.1:8000"
 // export const MOCK_USERNAME = "admin"
 // export const MOCK_PASSWORD = "1234567"
